e40d89fd8230a7757307f16ce1ced67d3a6c430d
jys0919 <jys880919@naver.com>
2025-06-02 15:10:17 +0900

    feat:Entity Student add

---
+++ src/main/java/fiveguys/edunet/domain/Student.java
@@ -0,0 +1,27 @@
+package fiveguys.edunet.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Student extends BaseTime{
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String username;
+    private String password;
+    private String studentname;
+    private String email;
+    private String phonenumber;
+        
+}
