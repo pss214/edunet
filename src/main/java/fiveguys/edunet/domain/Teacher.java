1218730a460adc4f5c49e593d9c0373a16067aa9
pss <ssp69597@gmail.com>
2025-06-11 14:46:57 +0900

    fix: Entity 수정

--- src/main/java/fiveguys/edunet/domain/Teacher.java
+++ src/main/java/fiveguys/edunet/domain/Teacher.java
@@ -7,14 +7,10 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -36,9 +32,6 @@ public class Teacher extends BaseTime implements UserDetails{
 
     private String email;
     private String phone;
-
-    @OneToOne(mappedBy = "teacher",cascade = CascadeType.ALL)
-    private Subject subject;
     public void changePassword(String encodedPw) {
         this.password = encodedPw;
     }
@@ -47,11 +40,5 @@ public class Teacher extends BaseTime implements UserDetails{
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of(new SimpleGrantedAuthority("ROLE_USER"));
-    }
-    public void setSubject(Subject subject){
-        this.subject=subject;
-        if (subject.getTeacher() != this) {
-            subject.setTeacher(this);
-        }
     }
 }
